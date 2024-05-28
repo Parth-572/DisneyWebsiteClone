84d95cef787374044ee4efc040d9ff237b69b601
Parth Makwana <parthmakwana572@gamil.com>
2024-05-28 20:20:29 +0530

    donotshowapikey

--- src/services/GlobalApi.jsx
+++ src/services/GlobalApi.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const movieBaseUrl = "https://api.themoviedb.org/3";
-const apiKey = "c79189b68161eb339d4fe6d7d06d7429";
+const apiKey = "";
 const movieByGenreBaseURl =
-  "https://api.themoviedb.org/3/discover/movie?api_key=c79189b68161eb339d4fe6d7d06d7429&with_genres=";
+  "https://api.themoviedb.org/3/discover/movie?api_key=api_keywith_genres=";
 
 const getTrendingVideos = axios.get(
   movieBaseUrl + "/trending/all/day?api_key=" + apiKey
